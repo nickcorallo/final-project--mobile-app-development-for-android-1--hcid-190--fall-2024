dd8f98d2368f3a473374594ddf72d59d404391a3
nickcorallo <corallo@hartford.edu>
2024-12-15 15:24:52 -0500

    make changes to sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.bothmatrixanddiamond"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.bothmatrixanddiamond"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
